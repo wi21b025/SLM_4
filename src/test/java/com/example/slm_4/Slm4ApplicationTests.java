88e98b0b6c0ecd46bb800fd9a5b0cb2eb69a2f3c
wi21b025 <wi21b025@technikum-wien.at>
2022-06-21 13:46:29 +0200

    Initial commit

---
+++ src/test/java/com/example/slm_4/Slm4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slm4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

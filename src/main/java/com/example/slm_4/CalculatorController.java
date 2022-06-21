56982fc8f3644546f30588fbba378a25c28aa074
wi21b025 <wi21b025@technikum-wien.at>
2022-06-21 14:28:42 +0200

    Sum

---
+++ src/main/java/com/example/slm_4/CalculatorController.java
@@ -0,0 +1,19 @@
+package com.example.slm_4;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+
+    @RequestMapping("/sum")
+    public int sum(
+            @RequestParam int a,
+            @RequestParam int b) {
+        return a + b;
+
+
+    }
+}
